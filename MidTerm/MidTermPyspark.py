eb391380036b4f44ebe83e791bc0dc666d359c69
VietDucFCB <116992108+VietDucFCB@users.noreply.github.com>
2024-11-29 23:33:17 +0700

    .

--- MidTerm/MidTermPyspark.py
+++ MidTerm/MidTermPyspark.py
@@ -44,7 +44,6 @@ channel_map = {
     'O1': 9, 'O2': 10, 'P8': 11, 'T8': 12, 'FC6': 13, 'F4': 14, 'F8': 15, 'AF4': 16
 }
 
-
 for mat_file in os.listdir(data_root):
     if mat_file.endswith('.mat'):
         states = read_mat_file(os.path.join(data_root, mat_file))
